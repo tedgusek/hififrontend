ef79f257a25e750b3be6d4b1dceb6edd26ce4bf7
Ted Gusek <theohgusek@gmail.com>
2024-03-21 12:05:33 -0400

    clean up

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -1,25 +1,3 @@
-// import { useState } from 'react';
-
-// interface SignOutProps {
-//   onSignOut: () => void;
-// }
-
-// const SignOutButton: React.FC<SignOutProps> = ({ onSignOut }) => {
-//   const handleSignOut = () => {
-//     // Perform any necessary sign-out logic
-//     // For example, clearing authentication tokens, resetting state, etc.
-//     // Then call the onSignOut callback provided by the parent component
-//     onSignOut();
-//   };
-
-//   return (
-//     <button onClick={handleSignOut} className="sign-out-button">
-//       Sign Out
-//     </button>
-//   );
-// };
-
-// export default SignOutButton;
 import React from 'react';
 
 interface SignOutButtonProps {
@@ -28,10 +6,13 @@ interface SignOutButtonProps {
 
 const SignOutButton: React.FC<SignOutButtonProps> = ({ onClick }) => {
   return (
-    <button onClick={onClick} className='border-white rounded-xl border-4 bg-purple-600 hover:bg-purple-300 py-2 px-6'>
+    <button
+      onClick={onClick}
+      className='border-white rounded-xl border-4 bg-purple-600 hover:bg-purple-300 py-2 px-6'
+    >
       Sign Out
     </button>
   );
-}
+};
 
 export default SignOutButton;
